9e0024abb3cc8a6b98c10abe2d9e89fb8f115c2a
ST10466818 <ST10466818@vcconnect.edu.za>
2025-05-18 23:37:11 +0200

    added the other 2 pages/screens

--- app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity2.kt
+++ app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity2.kt
@@ -1,25 +1,20 @@
 package za.co.varsitycollege.st10466818_assignment_drsdeck
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(/* view = */ findViewById(R.id.main)){ v, insets ->
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val textViewHead =findViewById<TextView>(R.id.textViewHead)
-        //app name
-
     }
 }
