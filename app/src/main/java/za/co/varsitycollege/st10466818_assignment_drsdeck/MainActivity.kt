94bc6c8745d805fe5a6d06b02bf1a2b26475bdb0
ST10466818 <ST10466818@vcconnect.edu.za>
2025-05-18 23:02:50 +0200

    adding heading to mainactivity .kt

--- app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity.kt
@@ -1,6 +1,7 @@
 package za.co.varsitycollege.st10466818_assignment_drsdeck
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(/* view = */ findViewById(R.id.main)){ v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textViewHead =findViewById<TextView>(R.id.textViewHead)
+        //app name
     }
 }
