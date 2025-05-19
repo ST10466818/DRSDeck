0f593354d2980a7afc651fff2b6cfc85fccc36ca
ST10466818 <ST10466818@vcconnect.edu.za>
2025-05-19 23:02:34 +0200

    sorting the second screen out

--- app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity.kt
+++ app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity.kt
@@ -1,8 +1,11 @@
 package za.co.varsitycollege.st10466818_assignment_drsdeck
 
+import android.content.Intent
+import android.os.Binder
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,16 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(/* view = */ findViewById(R.id.main)){ v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(/* view = */ findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val textViewHead =findViewById<TextView>(R.id.textViewHead)
-        //app name
+        val textViewHead = findViewById<TextView>(R.id.textViewHead)
+        val textViewIntro = findViewById<TextView>(R.id.textViewIntro)
         val startButton = findViewById<Button>(R.id.startButton)
-            //start buttton for the app
 
+        startButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity2.kt
+++ app/src/main/java/za/co/varsitycollege/st10466818_assignment_drsdeck/MainActivity2.kt
@@ -1,6 +1,9 @@
 package za.co.varsitycollege.st10466818_assignment_drsdeck
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,22 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var questonBox =findViewById<TextView>(R.id.questionBox)
+        //actual flashcard where questions will show up
+        var falseButton = findViewById<Button>(R.id.falseButton)
+        //Stay out means True
+        var trueButtton = findViewById<Button>(R.id.trueButton)
+        //Box,Box means False
+        var button3 = findViewById<Button>(R.id.button3)
+ 
+        falseButton.setOnClickListener{
+            Toast.makeText(this@MainActivity2, "False", Toast.LENGTH_SHORT
+            ).show()}
+        trueButtton.setOnClickListener{
+            Toast.makeText(this@MainActivity2, "True", Toast.LENGTH_SHORT
+            ).show()}
+        button3.setOnClickListener{
+            Toast.makeText(this@MainActivity2, "Next Page", Toast.LENGTH_SHORT
+            ).show()}
     }
 }
